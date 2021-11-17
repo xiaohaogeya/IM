e3ecb106a72f884b933e9f84d6ffebb958c934f1
xiaohao <19929238507@163.com>
2021-11-17 23:20:27 +0800

    【前端】：样式调整

---
+++ frontend/craco.config.js
@@ -0,0 +1,16 @@
+/* craco.config.js */
+
+const CracoAntDesignPlugin = require('craco-antd');
+
+module.exports = {
+    plugins: [
+        {
+            plugin: CracoAntDesignPlugin,
+            options: {
+                customizeTheme: {
+                    '@primary-color': '#1DA57A',
+                },
+            },
+        },
+    ],
+};
