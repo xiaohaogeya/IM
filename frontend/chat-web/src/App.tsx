f779438eaf4230bb4aa347fcd3febc9a8af67c20
xiaohao <19929238507@163.com>
2023-03-19 22:01:25 +0800

    前端

---
+++ frontend/chat-web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import type { FC } from 'react';
+import { Button } from 'antd';
+import 'antd/dist/reset.css';
+import './App.css';
+
+const App: FC = () => (
+    <div className="App">
+      <Button type="primary">Button</Button>
+    </div>
+);
+
+export default App;
